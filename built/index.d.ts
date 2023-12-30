e39f8e05cf3bfcb9f68c6e114939be4d5ad03cd2
tamaina <tamaina@hotmail.co.jp>
2023-12-30 11:56:13 +0000

    publish wip

--- built/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-import sharp from 'sharp';
-export declare function sharpBmp(source: Buffer | Int8Array | Uint8Array | Uint8ClampedArray | string, type: string, options?: sharp.SharpOptions): Promise<sharp.Sharp>;
