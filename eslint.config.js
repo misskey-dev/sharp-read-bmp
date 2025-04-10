4057552158d062de109cfcaf0429efc78937cdf0
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-04-10 19:15:10 +0900

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,9 +8,7 @@ export default [ // eslint-disable-line import/no-default-export
 	{
 		ignores: [
 			'**/node_modules',
-			'src/@types/package.json.d.ts',
 			'built',
-			'jest.config.js',
 			'test',
 		],
 	},
@@ -19,14 +17,10 @@ export default [ // eslint-disable-line import/no-default-export
 		languageOptions: {
 			parserOptions: {
 				parser: tsParser,
-				project: ['./tsconfig.json', './test/tsconfig.json'],
+				project: ['./tsconfig.json'],
 				sourceType: 'module',
 				tsConfigRootDir: import.meta.dirname,	
 			},
-		},
-		rules: {
-			// 空文字でもフォールバックしたいので無効
-			'@typescript-eslint/prefer-nullish-coalescing': 'off',
 		},
 	},
 	{
